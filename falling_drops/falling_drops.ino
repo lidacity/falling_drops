571155707138e9b4b4d8f3ee8fa23a5b850ce9c6
lidacity <lidacity@users.noreply.github.com>
2024-03-14 18:04:15 +0300

    rem
    изменил комментарии

--- falling_drops/falling_drops.ino
+++ falling_drops/falling_drops.ino
@@ -1,21 +1,16 @@
-//20512 66% 1861 90%
-//4042 13% 455 22%
-
 // Adafuit NeoPixel v1.12.0
 #include <Adafruit_NeoPixel.h>
  
 // ноги для каждой ленты подключать через резистор 470 Ом
 // питание лент от отдельного стабилизированного источника постоянного напряжения 5V
 // Земля общая с блоком питания Ардуино
-// ВНИМАНИЕ: потребление каждого кусочка ленты из 20 светодиодов = 6 Ватт (ток 1,2А на канал)
-// из 12 лент = 72 Ватт, что равно току почти 15А. если предполагается одновременное включение всех
-// практика показала что максимальное потребление одного отрезка ленты в режиме сосульки не более 200мА
+// Внимание: потребление одного цвета светодиода 0.012 А (12 мА), соответственно три светодиода (белый цвет) – 0.036 А (36 мА)
 
 #define COUNT_STRIP 12 // количество лент (максимальное значение = 12 или 8 в зависимости от количества дискретных пинов, + можно использовать расширитель портов ввода-вывода, например при использовании PCF8574 максимальное значение += 64)
-#define COUNT_PIXELS 20 // количество светодиодов в каждой ленте (максимальное значение = 255-LENGTH_METEORS)
+#define COUNT_PIXELS 25 // количество светодиодов в каждой ленте (максимальное значение = 255-LENGTH_METEORS)
 // использования памяти для NeoPixel = COUNT_STRIP * (40 + 3 * COUNT_PIXELS), не превышайте использование памяти вашего микроконтроллера
 
-#define LENGTH_METEORS 24 // длина метеора (максимальное значение = 255)
+#define LENGTH_METEORS 16 // длина метеора (максимальное значение = 255)
 #define INCREASE_BRIGHTNESS 256/LENGTH_METEORS
 
 const byte DATA_PINS[COUNT_STRIP] = { 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 }; // перечисление пинов, куда подключены ленты
@@ -26,63 +21,57 @@ const byte DATA_PINS[COUNT_STRIP] = { 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 }; 
 
 struct Strips
 {
-  Adafruit_NeoPixel Strip;
-  bool Enable;
-  int32_t Phase;
-  int32_t Pause;
-  int32_t Hue;
+  Adafruit_NeoPixel Strip; // адресная лента
+  bool Enable; // включение ленты
+  int32_t Phase; // позиция метеора на ленте
+  int32_t Pause; // задержка (в цикле loop) до следующей ленты
+  int32_t Hue; // цвет (тон) метеора
 };
 
-Strips Led[COUNT_STRIP]; // ленты
+Strips Led[COUNT_STRIP]; // массив лент
 
 
 void setup()
 {
-  // В случае управление питанием через ключ нужен этот кусок кода, чтобы включить ленту
-  // и дождаться окончания переходных процессов
-  /*pinMode(POWER_PIN, OUTPUT);
-  digitalprint(PIN_COLOR, HIGH);
-  delay(2000);*/
-  // инициализируем ленты
-  // стартовая инициализация всех массивов
+  // стартовая инициализация лент
   int32_t Hue = GetHue();
   for (int i=0; i<COUNT_STRIP; i++)
   {
     Led[i] = { Adafruit_NeoPixel(COUNT_PIXELS, DATA_PINS[i], NEO_GRB + NEO_KHZ800), true, 0, 0, Hue };
     Led[i].Strip.begin();
   }
-  // включение аналогового входа настройка диапазона цветов метеора
-//  pinMode(PIN_COLOR, INPUT);
-//  pinMode(PIN_DELAY, INPUT);
-//  pinMode(PIN_RAND, INPUT_PULLUP);
+  // включение дополнительных входов
+  pinMode(PIN_COLOR, INPUT);
+  pinMode(PIN_DELAY, INPUT);
+  pinMode(PIN_RAND, INPUT_PULLUP);
 }
  
  
 void loop()
 {
-  for (int i=0; i<COUNT_STRIP; i++) // поочередно перебираем все ленты
+  for (int i=0; i<COUNT_STRIP; i++) // перебор лент
   {
     if (Led[i].Enable) // если на ленте включён метеор
     {   
       Led[i].Strip.clear();
-      if (Led[i].Phase > COUNT_PIXELS + LENGTH_METEORS) // если метеор вылетел то
+      if (Led[i].Phase > COUNT_PIXELS + LENGTH_METEORS) // если метеор вылетел за пределы
       {
-        Led[i].Enable = false; // выключение ленты  
-        Led[i].Phase = 0; // сброс фазы ленты на НОЛЬ
-        Led[i].Pause = random(10, 50); // задание паузы до следующего включения (в циклах)
-        if (GetColor()) // если тумблер выключен
-          Led[i].Hue = GetHue(); // то работаем с резистором цвета
+        Led[i].Enable = false;
+        Led[i].Phase = 0;
+        Led[i].Pause = random(10, 50);
+        if (IsHue())
+          Led[i].Hue = GetHue(); // тон читается с потенциометра
         else
-          Led[i].Hue += 128; // иначе плавно меняем цвет        
+          Led[i].Hue += 128; // тон плавно увеличивается
       }
-      else // иначе засвечиваем Метеор на ленте
+      else // иначе рассчитывается метеор на ленте
       {
         int Index = Led[i].Phase;
         int32_t Hue = Led[i].Hue;
         int Val = 255;
         while ((Index >= 0) && (Val >= 0))
         {
-          if (Index < COUNT_PIXELS)
+          if (Index < COUNT_PIXELS) // метеор считается только в видимой части ленты
           {
             uint32_t Color = Led[i].Strip.ColorHSV(Hue, 255, Val);
             Led[i].Strip.setPixelColor(Index, Color);
@@ -90,11 +79,11 @@ void loop()
           Val -= INCREASE_BRIGHTNESS;
           Index--;
         }
-        Led[i].Phase++; // сдвигаем фазу
+        Led[i].Phase++;
       }
-      Led[i].Strip.show(); // засветка ленты
+      Led[i].Strip.show(); // отображение метеора на ленте
     }
-    else // если лента выключена
+    else // иначе на ленте метеор выключён
     {
       if (Led[i].Pause > 0)
         Led[i].Pause--; // то считаем длительность паузы
@@ -102,25 +91,24 @@ void loop()
         Led[i].Enable = true; // и по окончании включаем ленту снова
     }
   }
-  // скорость метеора если число + то скорость -
-  int StayTime = GetStay(); // снятие показателя задержки
-  delay(StayTime); // задержка по уровню скорости МЕТЕОРА
+  int StayTime = GetStay(); // чтение с потенциометра задержки
+  delay(StayTime);
 }
 
 
-bool GetColor()
+bool IsHue()
 {
-  return digitalRead(PIN_RAND) == HIGH; // если тумблер выключен
+  return digitalRead(PIN_RAND) == HIGH; // чтение выбора тона
 }
 
 
 int32_t GetHue()
 {
-  word Result = map(analogRead(PIN_COLOR), 0, 1023, 0, 65535); // в цветовом ряду HSV - hue описывается в диапазоне от 0 до 65535
-  return random(Result - 1024, Result + 1024); // с коридором флюктуации цвета
+  word Result = map(analogRead(PIN_COLOR), 0, 1023, 0, 65535); // в цветовом ряду HSV у NeoPixel - тон hue описывается в диапазоне от 0 до 65535
+  return random(Result - 1024, Result + 1024); // с коридором флюктуации тона
 }
 
 int GetStay()
 {
-  return map(analogRead(PIN_DELAY), 0, 1023, 5, 95); // снятие показателя задержки
+  return map(analogRead(PIN_DELAY), 0, 1023, 5, 95); // чтение показателя задержки
 }
